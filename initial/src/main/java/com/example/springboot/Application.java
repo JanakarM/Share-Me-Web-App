7712b486af35f857087c29af81c3c0448c6f87ba
Janakar <30556870+JanakarM@users.noreply.github.com>
2022-03-19 18:31:23 +0530

    Update Application.java

--- initial/src/main/java/com/example/springboot/Application.java
+++ initial/src/main/java/com/example/springboot/Application.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.boot.web.support.SpringBootServletInitializer
-import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @Configuration
 @EnableAutoConfiguration
