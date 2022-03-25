7ce773f7f48ccd3e94cbded805ed359fbf4bfdfc
Janakar M <janakar.m@zohocorp.com>
2022-03-25 18:40:52 +0530

    Externalization changes

--- initial/src/main/java/com/example/springboot/Application.java
+++ initial/src/main/java/com/example/springboot/Application.java
@@ -11,7 +11,6 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 // public class Application extends SpringBootServletInitializer {
 public class Application{
-
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
--- initial/src/main/java/com/example/springboot/HelloController.java
+++ initial/src/main/java/com/example/springboot/HelloController.java
@@ -2,13 +2,20 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
+// @RequestMapping("/external-config")
 public class HelloController {
-
+	@Value("${USER}")
+	String v;
+	@Value("${NAME}")
+	String n;
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!" + System.getenv("NAME");
+		// return "Greetings from Spring Boot!" + System.getenv("NAME");
+		return "Greetings from Spring Boot! " + v + "--" + n;
 	}
 	
 	// @GetMapping("/Hai")
