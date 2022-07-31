4c119bf97fec069c39374983d59c55be21da2797
Janakar M <janakar.muthuraj@gmail.com>
2022-07-31 15:21:37 +0530

    front-end build commands added and request filters added

--- rest_service/src/main/java/com/learning/rest_service/RestServiceApplication.java
+++ rest_service/src/main/java/com/learning/rest_service/RestServiceApplication.java
@@ -1,36 +1,12 @@
 package com.learning.rest_service;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.gson.GsonFactory;
-import com.learning.rest_service.component.BeanUtil;
-import com.learning.rest_service.model.User;
-import com.learning.rest_service.storage.database.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Collections;
 
 @SpringBootApplication
 public class RestServiceApplication {
 
 	public static void main(String[] args) throws Exception{
 		SpringApplication.run(RestServiceApplication.class, args);
-	}
-
-//	@Bean
-	public CommandLineRunner demo(UserRepository userRepository) {
-		return (args) -> {
-			// save a few customers
-			userRepository.save(new User("Jana", "jana@gmail.com", "http://google.com"));
-			userRepository.save(new User("Naveen", "naveen@gmail.com", "http://yahoo.com"));
-
-		};
 	}
 }
--- rest_service/src/main/java/com/learning/rest_service/security/filter_chain/ApplicationSecurityConfig.java
+++ rest_service/src/main/java/com/learning/rest_service/security/filter_chain/ApplicationSecurityConfig.java
@@ -6,8 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.RequestMatcherRedirectFilter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import java.net.http.HttpRequest;
 
 @Configuration
 @EnableWebSecurity
@@ -17,12 +24,17 @@ public class ApplicationSecurityConfig {
     public SecurityFilterChain createFilterChain(HttpSecurity http) throws Exception{
         return http.csrf(csrf-> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())).
                 authorizeHttpRequests(auth->
-                        auth.
-                                antMatchers("/user/login", "/").permitAll().
-                                antMatchers("/**").authenticated()
-//                                antMatchers("/feed").hasRole("USER").
-//                                antMatchers("/feed/").hasRole("ADMIN")
-                ).
-                build();
+                        auth
+                                .antMatchers("/user/login", "/").permitAll()
+                                .antMatchers("/static/**").permitAll()
+                                .antMatchers("/**").authenticated()
+//                                .antMatchers("/feed").hasRole("USER")
+//                                .antMatchers("/feed/").hasRole("ADMIN")
+                )
+                .addFilterBefore(new RequestMatcherRedirectFilter(ApplicationSecurityConfig::checkRedirection, "/"), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+    static boolean checkRedirection(HttpServletRequest request){
+        return !request.getRequestURI().equals("/") && !request.getRequestURI().equals("/user/login") && !request.getRequestURI().equals("/user/me") && !request.getRequestURI().contains("/static/") && request.getUserPrincipal() == null;
     }
 }
